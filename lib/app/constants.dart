6c12405d8b2a275b06ece6c261435b57bdf9ed6d
arafat <ahmed.arafat353@gmail.com>
2022-12-11 08:38:56 +0200

    add constants & extensions

---
+++ lib/app/constants.dart
@@ -0,0 +1,13 @@
+class Constants{
+  // base url
+  static const String baseUrl ="https://ahmedarafat.mocklab.io";
+
+  // methods
+  static const String login ="/customer/login";
+
+  // constanct
+  static const String empty ="";
+  static const int zero =0;
+
+
+}
